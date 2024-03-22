62d791d1fdce9423e133730347debc3524e0281b
Gregor Kielbasa <gregor.kielbasa@haeger-consulting.de>
2024-03-22 12:02:08 +0100

    all variables should be in camelCase

--- phase1/src/main/java/org/lager/exception/OrderIllegalIdException.java
+++ phase1/src/main/java/org/lager/exception/OrderIllegalIdException.java
@@ -2,6 +2,6 @@ package org.lager.exception;
 
 public class OrderIllegalIdException extends RuntimeException {
     public OrderIllegalIdException(long id) {
-        super("Order's id is invalid: " + id);
+        super("Order's ID is invalid: " + id);
     }
 }
--- phase1/src/main/java/org/lager/exception/OrderItemListNotPresentException.java
+++ phase1/src/main/java/org/lager/exception/OrderItemListNotPresentException.java
@@ -1,7 +1,7 @@
 package org.lager.exception;
 
 public class OrderItemListNotPresentException extends RuntimeException {
-    public OrderItemListNotPresentException(long ID) {
-        super("Order's List of Items is empty. Order ID: " + ID);
+    public OrderItemListNotPresentException(long id) {
+        super("Order's List of Items is empty. Order ID: " + id);
     }
 }
