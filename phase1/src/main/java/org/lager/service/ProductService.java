838190e2372db9b75ff795841084b1e0036ded5e
Gregor Kielbasa <gregor.kielbasa@haeger-consulting.de>
2024-03-26 16:43:11 +0100

    ProductService got methods to write/read to CSV

--- phase1/src/main/java/org/lager/service/ProductService.java
+++ phase1/src/main/java/org/lager/service/ProductService.java
@@ -2,18 +2,23 @@ package org.lager.service;
 
 import org.lager.exception.NoSuchProductException;
 import org.lager.model.Product;
+import org.lager.repository.ProductCsvEditor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.util.*;
 
 public class ProductService {
     private long newProductNumber = 100_000_000;
     private final Map<Long, Product> products;
+    private final ProductCsvEditor csvEditor;
     private final Logger logger = LoggerFactory.getLogger(ProductService.class);
 
-    public ProductService() {
+    public ProductService(ProductCsvEditor csvEditor) {
+        this.csvEditor = csvEditor;
         this.products = new HashMap<>();
+        loadFromFile();
     }
 
     public List<Product> getAll() {
@@ -24,6 +29,7 @@ public class ProductService {
         logger.debug("ProductService starts to insert new Product with {} ID and {} name", newProductNumber, newProductName);
         Product newProduct = new Product(newProductNumber, newProductName);
         products.put(newProductNumber, newProduct);
+        saveToFile();
         logger.debug("ProductService finished to insert new {} Product", newProductNumber);
         newProductNumber++;
         return newProduct;
@@ -42,6 +48,7 @@ public class ProductService {
     public void remove(long productNumber) {
         logger.info("ProductService removes {} Product", productNumber);
         products.remove(productNumber);
+        saveToFile();
     }
 
     public void rename(long productNumber, String productNewName) {
@@ -49,5 +56,30 @@ public class ProductService {
         Product product = search(productNumber)
                 .orElseThrow(() -> new NoSuchProductException(productNumber));
         product.setName(productNewName);
+        saveToFile();
+    }
+
+    private void saveToFile() {
+        try {
+            csvEditor.saveToFile(products.values().stream().toList());
+            logger.info("ProductService saved its state to CSV File");
+        } catch (IOException e) {
+            logger.error("ProductService was not able to save CSV File");
+        }
+    }
+
+    private void loadFromFile() {
+        try {
+            csvEditor.loadFromFile().forEach(customer -> products.put(customer.getNumber(), customer));
+            setNewCustomerNumber();
+            logger.info("ProductService loaded its state from CSV File");
+        } catch (IOException e) {
+            logger.warn("ProductService was not able to load CSV File");
+        }
+    }
+
+    private void setNewCustomerNumber() {
+        newProductNumber = products.keySet().stream().max(Long::compareTo).orElse(newProductNumber - 1);
+        newProductNumber++;
     }
 }
