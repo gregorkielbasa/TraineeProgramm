8770c9e426cae1fb617d203a6483571f18dd2309
Gregor Kielbasa <gregor.kielbasa@haeger-consulting.de>
2024-03-28 13:26:29 +0100

    CustomerService has been prepared to work with Repository

--- phase1/src/main/java/org/lager/service/CustomerService.java
+++ phase1/src/main/java/org/lager/service/CustomerService.java
@@ -2,53 +2,41 @@ package org.lager.service;
 
 import org.lager.exception.NoSuchCustomerException;
 import org.lager.model.Customer;
-import org.lager.repository.CustomerCsvEditor;
+import org.lager.repository.CustomerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
 import java.util.*;
 
 public class CustomerService {
-    private long newCustomerNumber = 100_000_000;
-    private final Map<Long, Customer> customers;
-    private final CustomerCsvEditor csvEditor;
+    private final CustomerRepository repository;
     private final Logger logger = LoggerFactory.getLogger(CustomerService.class);
 
-    public CustomerService(CustomerCsvEditor csvEditor) {
-        this.csvEditor = csvEditor;
-        this.customers = new HashMap<>();
-        loadFromFile();
-    }
-
-    public List<Customer> getAll() {
-        return new ArrayList<>(customers.values());
+    public CustomerService(CustomerRepository repository) {
+        this.repository = repository;
     }
 
     public Customer create(String newCustomerName) {
+        long newCustomerNumber = repository.getNextAvailableNumber();
         logger.debug("CustomerService starts to insert new Customer with {} ID and {} name", newCustomerNumber, newCustomerName);
         Customer newCustomer = new Customer(newCustomerNumber, newCustomerName);
-        customers.put(newCustomerNumber, newCustomer);
-        saveToFile();
+        repository.create(newCustomerNumber, newCustomer);
         logger.debug("CustomerService finished to insert new {} Customer", newCustomerNumber);
-        newCustomerNumber++;
         return newCustomer;
     }
 
     public Optional<Customer> search(long customerNumber) {
-        return Optional.ofNullable(customers.get(customerNumber));
+        return repository.read(customerNumber);
     }
 
-    public boolean validatePresence(long number) {
-        search(number)
-                .orElseThrow(() -> new NoSuchCustomerException(number));
-        return true;
+    public void validatePresence(long CustomerNumber) {
+        search(CustomerNumber)
+                .orElseThrow(() -> new NoSuchCustomerException(CustomerNumber));
     }
 
     public void remove(long customerNumber) {
         logger.info("CustomerService removes {} Customer", customerNumber);
-        customers.remove(customerNumber);
-        saveToFile();
+        repository.delete(customerNumber);
     }
 
     public void rename(long customerNumber, String customerNewName) {
@@ -56,31 +44,6 @@ public class CustomerService {
         Customer customer = search(customerNumber)
                 .orElseThrow(() -> new NoSuchCustomerException(customerNumber));
         customer.setName(customerNewName);
-        saveToFile();
-    }
-
-    private void saveToFile() {
-        try {
-            csvEditor.saveToFile(customers.values().stream().toList());
-            logger.info("CustomerService saved its state to CSV File");
-        } catch (IOException e) {
-            logger.error("CustomerService was not able to save CSV File");
-        }
-    }
-
-    private void loadFromFile() {
-        try {
-            customers.clear();
-            csvEditor.loadFromFile().forEach(customer -> customers.put(customer.getNumber(), customer));
-            setNewCustomerNumber();
-            logger.info("CustomerService loaded its state from CSV File");
-        } catch (IOException e) {
-            logger.warn("CustomerService was not able to load CSV File");
-        }
-    }
-
-    private void setNewCustomerNumber() {
-        newCustomerNumber = customers.keySet().stream().max(Long::compareTo).orElse(newCustomerNumber - 1);
-        newCustomerNumber++;
+        repository.update(customerNumber, customer);
     }
 }
