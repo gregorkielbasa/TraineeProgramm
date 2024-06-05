dd7d6f2374a7ddac259831b8860dbf6c2f8560e9
Gregor Kielbasa <gregor.kielbasa@haeger-consulting.de>
2024-06-05 13:50:15 +0200

    Spring WEB initial commit + Controllers has been created

---
+++ phase2/src/main/java/org/lager/controller/OrderController.java
@@ -0,0 +1,15 @@
+package org.lager.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+
+    @GetMapping("/")
+    public String test() {
+        return "hello word!";
+    }
+}
