c34bce214233309731b81e34a91d1dcdc94525da
Gregor Kielbasa <gregor.kielbasa@haeger-consulting.de>
2024-06-05 16:56:06 +0200

    ProductController got Exception support

--- phase2/src/main/java/org/lager/controller/ProductController.java
+++ phase2/src/main/java/org/lager/controller/ProductController.java
@@ -1,9 +1,13 @@
 package org.lager.controller;
 
+import org.lager.exception.NoSuchProductException;
+import org.lager.exception.ProductIllegalIdException;
+import org.lager.exception.ProductIllegalNameException;
 import org.lager.model.dto.ProductDto;
 import org.lager.service.ProductService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -26,19 +30,33 @@ public class ProductController {
     @PostMapping("/{newProductName}")
     @ResponseStatus(code = HttpStatus.CREATED)
     public ProductDto createProduct(@PathVariable String newProductName) {
-        return service.create(newProductName);
+        try {
+            return service.create(newProductName);
+        } catch (ProductIllegalNameException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage(), e);
+        } catch (ProductIllegalIdException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage(), e);
+        }
     }
 
     @GetMapping("/{productId}/")
     @ResponseStatus(code = HttpStatus.OK)
     public ProductDto getProduct(@PathVariable long productId) {
-        return service.get(productId);
+        try {
+            return service.get(productId);
+        } catch (NoSuchProductException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage(), e);
+        }
     }
 
     @PostMapping("/{productId}/{productNewName}")
     @ResponseStatus(code = HttpStatus.ACCEPTED)
     public ProductDto renameProduct(@PathVariable long productId, @PathVariable String productNewName) {
-        return service.rename(productId, productNewName);
+        try {
+            return service.rename(productId, productNewName);
+        } catch (NoSuchProductException | ProductIllegalNameException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage(), e);
+        }
     }
 
     @DeleteMapping("/{productId}/")
